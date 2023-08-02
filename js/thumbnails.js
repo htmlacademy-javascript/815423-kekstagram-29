9666125e649f21bd01cdc1c0c2fc363a98cbfbf8
Danny_KGZ <Dannykg1996@gmail.com>
2023-08-02 14:57:41 +0300

    отступ

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,31 +1,35 @@
-import {displayBigPhoto} from './modal-big-photos.js';
+import { displayBigPhoto } from "./modal-big-photos.js";
 
-const pictureContainerElement = document.querySelector('.pictures');
-const pictureTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
+const pictureContainerElement = document.querySelector(".pictures");
+const pictureTemplateElement = document
+  .querySelector("#picture")
+  .content.querySelector(".picture");
 
 const pictureListFragment = document.createDocumentFragment();
 
 const createThumbnail = (element) => {
+  const pictureElement = pictureTemplateElement.cloneNode(true);
 
-    const pictureElement = pictureTemplateElement.cloneNode(true);
+  const { url, description, likes, comments } = element;
+  const imgElement = pictureElement.querySelector(".picture__img");
+  imgElement.src = url;
+  imgElement.alt = description;
+  pictureElement.querySelector(".picture__likes").textContent = likes;
+  pictureElement.querySelector(".picture__comments").textContent =
+    comments.length;
+  pictureContainerElement.append(pictureElement);
 
-    const {url, description, likes, comments} = element;
-    const imgElement = pictureElement.querySelector('.picture__img');
-    imgElement.src = url;
-    imgElement.alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    pictureContainerElement.append(pictureElement);
-
-    pictureElement.addEventListener('click', () => {
-      displayBigPhoto(element);
-    });
+  pictureElement.addEventListener("click", () => {
+    displayBigPhoto(element);
+  });
 };
 
 const renderThumbnails = (drawThumbnails) => {
-  pictureContainerElement.querySelectorAll('.picture').forEach((element) => element.remove());
+  pictureContainerElement
+    .querySelectorAll(".picture")
+    .forEach((element) => element.remove());
   drawThumbnails.forEach((thumbnail) => createThumbnail(thumbnail));
   pictureContainerElement.append(pictureListFragment);
 };
 
-export {renderThumbnails};
+export { renderThumbnails };
